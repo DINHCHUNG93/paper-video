8d4361a98c5d592a5ac6fed723335ed95cd8eb25
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2026-04-12 16:34:53 +0530

    added analytics

--- platform/src/app/App.tsx
+++ platform/src/app/App.tsx
@@ -1,4 +1,5 @@
 import { RouterProvider } from 'react-router';
+import { Analytics } from '@vercel/analytics/react';
 import { router } from './routes';
 import { JobProvider } from './lib/JobContext';
 
@@ -6,6 +7,7 @@ export default function App() {
   return (
     <JobProvider>
       <RouterProvider router={router} />
+      <Analytics />
     </JobProvider>
   );
 }
